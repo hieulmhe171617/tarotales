7189f3bdc9a322403c76befe21cbae9cb5ce96ac
HaLBH <halbhhe176636@fpt.edu.vn>
2024-10-26 22:51:38 +0700

    Update UI

--- app/src/main/java/com/example/tarottales/activity/TopicDetailsActivity.java
+++ app/src/main/java/com/example/tarottales/activity/TopicDetailsActivity.java
@@ -9,6 +9,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,12 +18,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.tarottales.Database.DBContext;
 import com.example.tarottales.Database.TarotCardDAO;
@@ -83,7 +87,8 @@ public class TopicDetailsActivity extends AppCompatActivity {
     }
 
     private void onClickAi(View view) {
-        String[] topics = {"Tình yêu", "Sự nghiệp", "Sức khỏe","Tinh thần","Khác"};
+
+        String[] topics = {"Tình cảm", "Sự nghiệp", "Sức khỏe","Tinh thần","Khác"};
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Bạn muốn hỏi về chủ đề nào?");
         builder.setItems(topics, new DialogInterface.OnClickListener() {
@@ -105,11 +110,18 @@ public class TopicDetailsActivity extends AppCompatActivity {
                         .replace(R.id.main, chatFragment)
                         .addToBackStack(null)
                         .commit();
+                hiddenButon();
             }
+
         });
         builder.show();
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        displayButon();
+    }
 
     private void onClickViewHistory(View view) {
         //mo sang intent lich su
@@ -244,4 +256,13 @@ public class TopicDetailsActivity extends AppCompatActivity {
         //
         dbContext = new TarotCardDAO(this);
     }
+    private void hiddenButon(){
+        btnAi.setVisibility(View.GONE);
+        btnSave.setVisibility(View.GONE);
+    }
+    public void displayButon(){
+        btnAi.setVisibility(View.VISIBLE);
+        btnSave.setVisibility(View.VISIBLE);
+    }
+
 }
